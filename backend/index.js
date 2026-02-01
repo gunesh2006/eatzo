c44d4da70a3cc6abe5983b4c386384f6d8a65ca9
Gunesh Sharma <guneshsharma51@gmail.com>
2026-02-01 12:54:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const server = http.createServer(app); //http server
 const io = new Server(server, {
   //websocket server
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://eatzo-frontend.onrender.com",
     credentials: true,
     methods: ["POST", "GET"],
   },
@@ -29,7 +29,7 @@ app.set("io", io); // to access io from anywhere in backend
 connectDb();
 
 const port = process.env.PORT || 5000;
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://eatzo-frontend.onrender.com", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRouter);
