840ad515dd3db6a669811e229cbf06ae2319c589
Gunesh Sharma <guneshsharma51@gmail.com>
2026-02-01 13:18:48 +0530

    Update CheckOutPage.jsx

--- frontend/src/pages/CheckOutPage.jsx
+++ frontend/src/pages/CheckOutPage.jsx
@@ -18,14 +18,7 @@ import { PayPalButtons } from "@paypal/react-paypal-js";
 import L from "leaflet";
 
 
-// Fix default icon paths
-delete L.Icon.Default.prototype._getIconUrl;
 
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-  iconUrl: require("leaflet/dist/images/marker-icon.png"),
-  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-});
 const CheckOutPage = () => {
   const { location, address } = useSelector((state) => state.map);
   const { cartItems, totalAmount } = useSelector((state) => state.user);
